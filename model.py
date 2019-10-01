313135fa8f80d20e0588a22b51278c2706926d5b
dungbachviet <dungbachviet@gmail.com>
2019-10-01 20:02:52 +0700

    Update model.py

--- model.py
+++ model.py
@@ -53,6 +53,7 @@ class upsample_conv(nn.Module):
         return out
 
 # ??? Shortcut for what. What is the purpose of 'shortcut + out' ???
+# ??? Why don't use batch-norm, ReLU
 class residualBlock_down(nn.Module): # for discriminator, no batchnorm
     def __init__(self, inplanes, outplanes):
         super(residualBlock_down, self).__init__()
@@ -60,7 +61,8 @@ class residualBlock_down(nn.Module): # for discriminator, no batchnorm
         self.conv1 = nn.Conv2d(inplanes, outplanes, 3, 1, 1)
         self.conv2 = conv_mean_pool(outplanes, outplanes)
         self.ReLU = nn.ReLU()
-
+    
+    # Why need to let x put in 2 path (shortcut and out), afterwards add both of them into one ???
     def forward(self, x):
         shortcut = self.conv_shortcut(x)
         out = x
@@ -69,9 +71,9 @@ class residualBlock_down(nn.Module): # for discriminator, no batchnorm
         out = self.ReLU(out)
         out = self.conv2(out)
 
-        return shortcut + out
+        return shortcut + out # ??? What meaning
 
-
+# For generator, using batch-norm and ReLU
 class residualBlock_up(nn.Module):
     def __init__(self, inplanes, outplanes):
         super(residualBlock_up, self).__init__()
@@ -91,24 +93,24 @@ class residualBlock_up(nn.Module):
         out = self.bn2(out)
         out = self.ReLU(out)
         out = self.conv2(out)
-
         return shortcut + out
 
-
+# Encoder: Image x --> View vector v + Latent vector z
 class _G_xvz(nn.Module):
     def __init__(self):
         super(_G_xvz, self).__init__()
         #self.conv = nn.Conv2d(3, 64, 3, 1, 1) 64*64 resolution implementation
         self.conv = nn.Conv2d(3, 64, 3, 1, 1) # 3*128*128 --> 64*128*128
-        self.resBlock0 = residualBlock_down(64, 64) # 64*128*128 --> 64*64*64
-        self.resBlock1 = residualBlock_down(64, 128)
-        self.resBlock2 = residualBlock_down(128, 256)
-        self.resBlock3 = residualBlock_down(256, 512)
-        self.resBlock4 = residualBlock_down(512, 512)
-        self.fc_v = nn.Linear(512*4*4, v_siz)
-        self.fc_z = nn.Linear(512*4*4, z_siz)
-        self.softmax = nn.Softmax()
+        self.resBlock0 = residualBlock_down(64, 64) # 64*128*128 --> 64*64*64, reduce the size of each side 2 times
+        self.resBlock1 = residualBlock_down(64, 128) # 64*64*64 --> 128*32*32
+        self.resBlock2 = residualBlock_down(128, 256) # 128*32*32 --> 256*16*16
+        self.resBlock3 = residualBlock_down(256, 512) # 256*16*16 --> 512*8*8
+        self.resBlock4 = residualBlock_down(512, 512) # 512*8*8 --> 512*4*4 (512 feature maps of size (4,4))
+        self.fc_v = nn.Linear(512*4*4, v_siz) # Fully-connected layer from 512*4*4 to 9 (view vector v)
+        self.fc_z = nn.Linear(512*4*4, z_siz) # Fully-connected layer from 512*4*4 to (128-9) (latent vector z) 
+        self.softmax = nn.Softmax() # Softmax for converting a vector to a distribution (each in vector have a range of (0,1)) 
     
+    # Specify the flow of architecture
     def forward(self, x):
         out = self.conv(x)
         out = self.resBlock0(out)
@@ -116,14 +118,14 @@ class _G_xvz(nn.Module):
         out = self.resBlock2(out)
         out = self.resBlock3(out)
         out = self.resBlock4(out)
-        out = out.view(-1, 512*4*4)
+        out = out.view(-1, 512*4*4) # Flatten from 512 feature maps of size (4,4) to vector of 512*4*4 features (used for fully-connected layer)
         v = self.fc_v(out)
         v = self.softmax(v)
         z = self.fc_z(out)
-
         return v, z
 
-
+# Generator: View vector v + Laten vector z ==> Image x
+# Each layer in generator architecture always use batch-norm and ReLU. Why don't use in Encoder (G_xvz) and Discriminator (D_xvs)
 class _G_vzx(nn.Module):
     def __init__(self):
         super(_G_vzx, self).__init__()
@@ -142,22 +144,21 @@ class _G_vzx(nn.Module):
         self.tanh = nn.Tanh()
 
     def forward(self, v, z):
-        x = torch.cat((v,z), 1)
-        out = self.fc(x) # out: 512*4*4
-        out = out.view(-1, 512, 4, 4) # (-1, 512, 4, 4)
+        x = torch.cat((v,z), 1) # concatenate 2 vector into 1 vector
+        out = self.fc(x) # out: 512*4*4, (self.fc is aware of mini-batch for processing)
+        out = out.view(-1, 512, 4, 4) # (-1, 512, 4, 4), -1 denote the remaining size of mini-batch
         out = self.resBlock1(out)
         out = self.resBlock2(out)
         out = self.resBlock3(out)
         out = self.resBlock4(out)
-        out = self.resBlock5(out)
+        out = self.resBlock5(out) # (-1, 3, 128, 128)
         out = self.bn(out)
         out = self.ReLU(out)
         out = self.conv(out)
         out = self.tanh(out)
-
         return out
 
-
+# Discriminator: Image x ==> View distribution v + Quality score s
 class _D_xvs(nn.Module):
     def __init__(self):
         super(_D_xvs, self).__init__()
@@ -179,9 +180,9 @@ class _D_xvs(nn.Module):
         x = self.resBlock2(x)
         x = self.resBlock3(x)
         x = self.resBlock4(x)
-        x = x.view(-1, 512*4*4)
-        v = self.fc_v(x)
-        v = self.softmax(v)
-        s = self.fc_s(x)
+        x = x.view(-1, 512*4*4) # Flaten 512 feature maps of size (4,4) to one vector of size 512*4*4
+        v = self.fc_v(x) # Fully-connected layer for converting from 512*4*4 to 9 (view vector)
+        v = self.softmax(v) # Convert view vector to a distribution
+        s = self.fc_s(x) # Fully-connected layer for converting from 512*4*4 to 1 (quality score)
 
         return v, s
